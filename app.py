5efad5e1568d4919665d1fe2361b15557c8c511f
Fredrik Welander <welandfr@arcada.fi>
2024-03-22 15:21:45 +0200

    add code

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_cors import CORS 
+
+"""
+Kör appen enligt följande:
+    1. Öppna en terminal och installera requirements med
+        pip install -r requirements.txt
+    2. Ändra PORT-konstanten till rätt portnummer enligt lista på itslearning
+    3. Starta appen i terminalen med
+        python app.py
+    4. Besök sidan på nätet med https://serverns-url:80xx
+"""
+
+PORT=5000
+app = Flask(__name__)
+CORS(app) # Tillåt cross-origin requests
+
+@app.route("/")
+def hello():
+    return "<h1>Hello, World!</h1>"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=PORT, debug=True, ssl_context=(
+        '/etc/letsencrypt/fullchain.pem', 
+        '/etc/letsencrypt/privkey.pem'
+    ))
