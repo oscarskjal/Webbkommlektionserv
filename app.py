4e7c483d15fec786cfcc0ce12a7d97f7ee940ade
oscarskjal <oscarskjal2@gmail.com>
2024-04-18 08:26:56 +0000

    14.04

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS 
 
-PORT=5000
+PORT=8341
 
 app = Flask(__name__)
 CORS(app) # Tillåt cross-origin requests
 
-@app.route("/")
+@app.route("/", methods = ['GET', 'POST'])
 def hello():
-    return "<h1>Hello, World!</h1>"
+    return { 'greeting': "hello, flask-JSON",
+'method' : request.method}
+
+@app.route("/test", methods = ['GET', 'POST'])
+def test():
+    return { "msg": "testing",
+'method' : request.method}
+    
+
+@app.route("/ip")
+def ip():
+
+    ip = request.remote_addr
+    return { "ip": ip }
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=PORT, debug=True, ssl_context=(
